075dcab030631152d88df28247b99b15a830aaac
quan <quan@quan.local>
2016-07-01 03:34:58 +0800

    2016-7-1 --- 初始化

---
+++ jianshuDemo/jianshuDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jianshuDemo
+//
+//  Created by quan on 16/7/1.
+//  Copyright © 2016年 quan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jianshuDemo/jianshuDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  jianshuDemo
+//
+//  Created by quan on 16/7/1.
+//  Copyright © 2016年 quan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
