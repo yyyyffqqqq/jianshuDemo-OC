d61379c241d0d2354d6f8f7cb6d4b194919a85dc
quan <quan@quan.local>
2016-07-01 18:14:15 +0800

    2016--7-1   添加tabbarcontroller框架及其各个子视图；

---
+++ jianshuDemo/View/CustomTabBar/MyCustomTabBar.h
@@ -0,0 +1,18 @@
+//
+//  MyCustomTabBar.h
+//  jianshuDemo
+//
+//  Created by quan on 16/7/1.
+//  Copyright © 2016年 quan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyCustomTabBar : UITabBar
+
+/** 发布按钮 */
+@property (nonatomic, weak) UIButton *publishButton;
+
+//@property (nonatomic, assign) void (^publishBtTapCallback)();
+
+@end
---
+++ jianshuDemo/View/CustomTabBar/MyCustomTabBar.m
@@ -0,0 +1,85 @@
+//
+//  MyCustomTabBar.m
+//  jianshuDemo
+//
+//  Created by quan on 16/7/1.
+//  Copyright © 2016年 quan. All rights reserved.
+//
+
+#import "MyCustomTabBar.h"
+
+#import "PublishViewController.h"
+
+
+@implementation MyCustomTabBar
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    if (self = [super initWithFrame:frame]) {
+        // 设置背景图片
+//        self.backgroundImage = [UIImage imageNamed:@"tabbar-light"];
+        
+        // 添加发布按钮
+        UIButton *publishButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [publishButton setBackgroundImage:[UIImage imageNamed:@"button_write"] forState:UIControlStateNormal];
+        [publishButton setBackgroundImage:[UIImage imageNamed:@"button_write"] forState:UIControlStateHighlighted];
+        [publishButton sizeToFit];
+        [publishButton addTarget:self action:@selector(publishClick) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:publishButton];
+        self.publishButton = publishButton;
+    }
+    return self;
+}
+
+- (void)publishClick
+{
+    //    if (_publishBtTapCallback) {
+    //        self.publishBtTapCallback();
+    //    }
+    
+    PublishViewController *publish = [[PublishViewController alloc] init];
+    UINavigationController *publishNV = [[UINavigationController alloc]initWithRootViewController:publish];
+    [self.window.rootViewController presentViewController:publishNV animated:YES completion:nil];
+    
+}
+
+/**
+ * 布局子控件
+ */
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    // tabBar的尺寸
+    CGFloat width = self.frame.size.width;
+    CGFloat height = self.frame.size.height;
+    
+    // 设置发布按钮的位置
+    self.publishButton.center = CGPointMake(width * 0.5, height * 0.5);
+    
+    // 按钮索引
+    int index = 0;
+    
+    // 按钮的尺寸
+    CGFloat tabBarButtonW = width / 5;
+    CGFloat tabBarButtonH = height;
+    CGFloat tabBarButtonY = 0;
+    
+    // 设置4个TabBarButton的frame
+    for (UIView *tabBarButton in self.subviews) {
+        if (![NSStringFromClass(tabBarButton.class) isEqualToString:@"UITabBarButton"]) continue;
+        
+        // 计算按钮的X值
+        CGFloat tabBarButtonX = index * tabBarButtonW;
+        if (index >= 2) { // 给后面2个button增加一个宽度的X值
+            tabBarButtonX += tabBarButtonW;
+        }
+        
+        // 设置按钮的frame
+        tabBarButton.frame = CGRectMake(tabBarButtonX, tabBarButtonY, tabBarButtonW, tabBarButtonH);
+        // 增加索引
+        index++;
+    }
+}
+
+@end
